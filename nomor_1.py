aff52104d6284f08f71eaa554d785431b10aa5de
zufarjw <168433608+zufarjw@users.noreply.github.com>
2024-04-29 20:08:39 +0700

    Add files via upload

---
+++ nomor_1.py
@@ -0,0 +1,9 @@
+# EXAMPLES
+
+# Input
+x = int(input(f"Masukan angka  : "))
+# Expected result
+if x % 2 == 0:
+    print(f"Angka {x} adalah bilangan genap")
+else :
+   print(f"Angka {x} adalah bilangan ganjil") 
---
+++ nomor_4.py
@@ -0,0 +1,12 @@
+import numpy as np
+np.set_printoptions
+
+A= np.array([
+    [98, 88, 78],
+    [77, 78, 79],
+    [66, 67, 68],
+    
+], float
+)
+
+print("Matriks = ",A)
